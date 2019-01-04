cc9ed03c3c69f7342126f281a6d631586a2f4cdb
marcincyborants <marcin.cyboran@testarmy.com>
2019-01-04 10:39:47 +0100

    2 brands

--- scripts.js
+++ scripts.js
@@ -1,18 +1,28 @@
 (function ($) {
-
   $(document).ready(function () {
-
     var form = $("#form-generator");
-
     form.on('submit', function (event) {
 
       $.each(form.serializeArray(), function (i, field) {
+
+        console.log(field);
 
         var fieldname = field.name + "_field";
         if (field.value != "") {
 
           if (fieldname.indexOf("social") >= 0) {
             $("#" + fieldname).attr("src", field.value);
+
+          } else if (fieldname.indexOf("brand") >= 0) {
+            var socialLink = "https://www.linkedin.com/company/testarmy/";
+            var logoLink = "https://raw.githubusercontent.com/marcincyborants/CyberForces-generator/gh-pages/testarmy_logo.png";
+            if (field.value == "cyberforces") {
+              socialLink = "https://www.linkedin.com/company/cyberforces/";
+              logoLink = "https://raw.githubusercontent.com/marcincyborants/CyberForces-generator/gh-pages/cyberforces_logo_dark.png";
+            }
+            $("#linkedin-social").attr("href", socialLink);
+            $("#logo_field").attr("href", logoLink);
+
           } else {
             $("#" + fieldname).text(field.value);
           }
